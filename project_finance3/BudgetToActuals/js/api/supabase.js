b208dc3936218442e81f84638e1a7f7fadb9dc56
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-11 18:20:05 -0500

    Update supabase.js

--- project_finance3/BudgetToActuals/js/api/supabase.js
+++ project_finance3/BudgetToActuals/js/api/supabase.js
@@ -1,8 +1,8 @@
 
 // js/api/supabase.js
 // CDN version — no import, use global supabase
-const SUPABASE_URL = 'https://yonpinjixytqooqyyzdh.supabase.co'; // your URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvbnBpbmppeHl0cW9vcXl5emRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNjkwMzIsImV4cCI6MjA3NDk0NTAzMn0.8g9iNl4kmIm77u7TT8cylgcV872D45pzZGHJWBnZBGo';
+const SUPABASE_URL = 'https://maisrqextcioxwfbghoy.supabase.co'; // your URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXNycWV4dGNpb3h3ZmJnaG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3Njk1OTksImV4cCI6MjA3ODM0NTU5OX0.TNUJnkwxfRQI_Lv6Xn5pvSXWTAWqM7ynW13ndVkFA_M';
 
 let supabaseClient = null;
 
