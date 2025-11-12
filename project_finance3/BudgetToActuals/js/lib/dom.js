b7091f52553a795bb00988e625b7cced72ed23ed
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-12 10:57:31 -0500

    Update dom.js

--- project_finance3/BudgetToActuals/js/lib/dom.js
+++ project_finance3/BudgetToActuals/js/lib/dom.js
@@ -1,7 +1,2 @@
-
 export const $  = (sel, el=document) => el.querySelector(sel);
 export const $$ = (sel, el=document) => [...el.querySelectorAll(sel)];
-export const h  = (html) => {
-  const t = document.createElement('template'); t.innerHTML = html.trim(); 
-  return t.content.firstElementChild;
-};
