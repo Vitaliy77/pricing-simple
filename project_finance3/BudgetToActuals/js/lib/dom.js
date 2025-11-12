30ed278a7707834d1bb9610156753c0e59719dd9
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-12 14:04:18 -0500

    Update dom.js

--- project_finance3/BudgetToActuals/js/lib/dom.js
+++ project_finance3/BudgetToActuals/js/lib/dom.js
@@ -1,10 +1,4 @@
 // /js/lib/dom.js
 export const $  = (sel, el = document) => el.querySelector(sel);
 export const $$ = (sel, el = document) => [...el.querySelectorAll(sel)];
-
-// tiny helper: build a DOM node from an HTML string
-export const h = (html) => {
-  const t = document.createElement('template');
-  t.innerHTML = html.trim();
-  return t.content.firstElementChild;
-};
+export const h  = (html) => { const t = document.createElement('template'); t.innerHTML = html.trim(); return t.content.firstElementChild; };
