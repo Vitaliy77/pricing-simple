f50e8e7b40861d0527441b36ec7ca6645c5e5682
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-12 10:54:49 -0500

    Update supabase.js

--- project_finance3/BudgetToActuals/js/api/supabase.js
+++ project_finance3/BudgetToActuals/js/api/supabase.js
@@ -1,12 +1,9 @@
-
-// js/api/supabase.js
-// CDN version — no import, use global supabase
-
+// //js/api/supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const client = createClient(
-  const SUPABASE_URL = 'https://maisrqextcioxwfbghoy.supabase.co'; // your URL
-  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXNycWV4dGNpb3h3ZmJnaG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3Njk1OTksImV4cCI6MjA3ODM0NTU5OX0.TNUJnkwxfRQI_Lv6Xn5pvSXWTAWqM7ynW13ndVkFA_M';
- 
-  { auth: { persistSession: true, autoRefreshToken: true } }
-);
+const SUPABASE_URL = "https://maisrqextcioxwfbghoy.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXNycWV4dGNpb3h3ZmJnaG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3Njk1OTksImV4cCI6MjA3ODM0NTU5OX0.TNUJnkwxfRQI_Lv6Xn5pvSXWTAWqM7ynW13ndVkFA_M";
+
+export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: { persistSession: true, autoRefreshToken: true },
+});
