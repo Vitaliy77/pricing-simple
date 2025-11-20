5a68a6311fca2d676b333e930dcb94bd30fba9a3
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-19 22:52:38 -0500

    Update supabase.js

--- TE_app/js/api/supabase.js
+++ TE_app/js/api/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://ugsedypndgtidbokttbk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXNycWV4dGNpb3h3ZmJnaG95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3Njk1OTksImV4cCI6MjA3ODM0NTU5OX0.TNUJnkwxfRQI_Lv6Xn5pvSXWTAWqM7ynW13ndVkFA_M";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnc2VkeXBuZGd0aWRib2t0dGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MTAxNDgsImV4cCI6MjA3OTE4NjE0OH0.cHwzZ7UcfbD0-vq5oedbACvH_wMzCEZFqc-_S1neDcU";
 
 export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true },
