23cd1d921a6048ba564bc790762061089cf37ff8
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-22 12:21:58 -0500

    Update tailwind.config.js

--- project_finance3/BudgetToActuals/tailwind.config.js
+++ project_finance3/BudgetToActuals/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./js/**/*.js",
+    "./index.html",                    // ← crucial
+    "./js/**/*.{js,html}",             // all your tab files
+    "./css/styles.css",                // if you have custom CSS
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
