bfa3d06ad39c515ea87262fe6712b7eb4cc31b60
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-27 11:56:08 -0500

    Create costBudget.js

---
+++ project_finance3/budget_projects/js/tabs/costBudget.js
@@ -0,0 +1,17 @@
+// js/tabs/costBudget.js
+export const template = /*html*/ `
+  <article>
+    <h3>Cost Budget</h3>
+    <p>
+      Here we will enter employee hours (converted to cost), subcontractor dollars,
+      and other direct costs by month. Fields should accept up to 7-digit whole dollars.
+    </p>
+  </article>
+`;
+
+export const costBudgetTab = {
+  template,
+  init() {
+    // TODO: implement cost entry grid and modals for adding employees, vendors, cost types
+  },
+};
