ce0a9bffc481e1efb517e2f870feff44d0bdd0a7
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-27 11:55:10 -0500

    Create summaryPlan.js

---
+++ project_finance3/budget_projects/js/tabs/summaryPlan.js
@@ -0,0 +1,14 @@
+// js/tabs/summaryPlan.js
+export const template = /*html*/ `
+  <article>
+    <h3>Summary Financial Plan</h3>
+    <p>Coming next: monthly P&amp;L, charts, and “submit final” workflow.</p>
+  </article>
+`;
+
+export const summaryPlanTab = {
+  template,
+  init() {
+    // TODO: aggregate planning_lines into monthly P&L and charts
+  },
+};
