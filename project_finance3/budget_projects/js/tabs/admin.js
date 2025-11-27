d0da771bc8c29c3ee59508e9be3e343775f2c64e
vitaliy ulitovsky <vitaliy.ulitovsky@gmail.com>
2025-11-27 11:56:28 -0500

    Create admin.js

---
+++ project_finance3/budget_projects/js/tabs/admin.js
@@ -0,0 +1,17 @@
+// js/tabs/admin.js
+export const template = /*html*/ `
+  <article>
+    <h3>Admin</h3>
+    <p>
+      Admin view for editing raw tables (projects, employees, new_hires, vendors, etc.).
+      We can later build a generic data grid here.
+    </p>
+  </article>
+`;
+
+export const adminTab = {
+  template,
+  init() {
+    // TODO: generic CRUD UI for main tables
+  },
+};
